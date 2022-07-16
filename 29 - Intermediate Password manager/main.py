cb025ff064e23f315df699a236e963215c30f20b
pedrolisboaa <phlisboa2000@gmail.com>
2022-07-15 22:54:32 -0300

    day 30

--- 29 - Intermediate Password manager/main.py
+++ 29 - Intermediate Password manager/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -18,21 +19,58 @@ def generator_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def create_data():
-    site = website_input.get()
+    site = website_input.get().upper()
     email = email_user_input.get()
     password = password_input.get()
+    new_data = {
+        site: {
+            'email': email,
+            'password': password,
+        }
+    }
 
     if len(site) == 0 or len(email) == 0 or len(password) == 0:
         messagebox.showinfo(title='Erroooooor!!', message='All fields must be completed')
     else:
-        is_ok = messagebox.askokcancel(title=site, message=f'Gostaria de salvar os dados:\n{email} \n{password}')
+        try:
+            with open('data.json', 'r') as f:
+                # Reading old data
+                data = json.load(f)
+                # Updating old data with new dataa
+                data.update(new_data)
 
-        if is_ok:
-            with open('data.txt', 'a', encoding='utf-8') as f:
-                text = f'{site} | {password} | {email}'
-                f.write(f'{text}\n')
+            with open('data.json', 'w', encoding='utf-8') as f:
+                # Saving update data
+                json.dump(data, f, indent=4)
 
-        clear_text()
+
+        except FileNotFoundError:
+            with open('data.json', 'w') as f:
+                json.dump(new_data, f, indent=4)
+        finally:
+            clear_text()
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_input.get().upper()
+
+    try:
+        with open('data.json', 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title='Error!', message=f'No Data File found.')
+    else:
+        if website in data:
+            email = data[website]['email']
+            password = data[website]['password']
+            messagebox.showinfo(title=website.capitalize(), message=f'Email: {email}\nPassword: {password}')
+        else:
+            messagebox.showinfo(title='Error!', message=f'Does not exist {website.capitalize()}')
+
+
+
+
 
 
 # ---------------------------- CLEAR TEXT ------------------------------- #
@@ -57,9 +95,12 @@ canvas.grid(column=1, row=0)
 website_text = Label(text='Website:', bg='WHITE')
 website_text.grid(column=0, row=1)
 
-website_input = Entry(width=36)
-website_input.grid(column=1, row=1, columnspan=2)
+website_input = Entry(width=21)
+website_input.grid(column=1, row=1)
 website_input.focus()
+
+search = Button(text='Search', bg='WHITE', command=find_password)
+search.grid(column=2, row=1)
 
 # Email usermane
 email_user = Label(text='Email/Username:', bg='WHITE')
