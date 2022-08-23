07b83c26c705b80ecf117da6955a6719f925f818
pedrolisboaa <phlisboa2000@gmail.com>
2022-08-23 17:47:22 -0300

    opa

--- 46 - Web Scraping e spotify VOLTAR/teste.py
+++ 46 - Web Scraping e spotify VOLTAR/teste.py
@@ -5,8 +5,8 @@ sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         scope="playlist-modify-private",
         redirect_uri="http://localhost:8888/callback",
-        client_id='b5ba5d19d7754427a1481ce932ef5a3c',
-        client_secret='dde775c0d5844f5a9822346f638a8f5b',
+        client_id=""
+        client_secret="",
         show_dialog=True,
         cache_path="token.txt"
     )
